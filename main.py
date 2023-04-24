9306a48669aeeb9e704078bdd7876bc83e75fe1f
PaulaMachado <128530844+paulaRMachado@users.noreply.github.com>
2023-04-24 20:26:35 +0200

    Delete main.py
    Wrong upload

--- main.py
+++
@@ -1,7 +0,0 @@
-import src.aquire_data as aquire
-import src.clean as clean
-import src.visualization as viz
-
-df = aquire.aquire_data("global_shark_attacks")
-df_clean = clean.basic_cleaning(df)
-viz.visualizing(df_clean)
